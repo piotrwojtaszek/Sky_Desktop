016a470258110ad201770ff20bfdc789ad7a551b
piotrwojtaszek <37346684+pietrek15@users.noreply.github.com>
2019-06-11 20:50:54 +0200

    some basics stuff

--- Assets/CameraRotate.cs
+++ Assets/CameraRotate.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CameraRotate : MonoBehaviour
 {
+    [SerializeField] private Vector2 speed;
+    private float yaw, pitch;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,17 @@ public class CameraRotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        RotateCamera();
+    }
+
+    void RotateCamera()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            yaw -= speed.x * Input.GetAxis("Mouse X");
+            pitch += speed.y * Input.GetAxis("Mouse Y");
+            pitch = Mathf.Clamp(pitch, -90f, 90f);
+            transform.eulerAngles = new Vector3(pitch, yaw, 0f);
+        }
     }
 }
--- Assets/ChangeSky.cs
+++ Assets/ChangeSky.cs
@@ -7,34 +7,19 @@ public class ChangeSky : MonoBehaviour
 
     [SerializeField] private Material sky;
     [SerializeField] private Texture[] images;
-    [SerializeField] private Transform cam;
-    [SerializeField] private Vector2 speed;
     private int currentImage;
-    private float yaw, pitch;
+ 
     // Start is called before the first frame update
     void Start()
     {
        RenderSettings.skybox = sky;
-        cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
         LoadNext();
-        RotateCamera();
         
-    }
-
-    void RotateCamera()
-    {
-        if (Input.GetKey(KeyCode.Mouse0))
-        {
-            yaw -= speed.x * Input.GetAxis("Mouse X");
-            pitch += speed.y * Input.GetAxis("Mouse Y");
-            pitch = Mathf.Clamp(pitch, -90f, 90f);
-            cam.eulerAngles = new Vector3(pitch, yaw, 0f);
-        }
     }
 
     void LoadNext()
