3501ab4502b1c83425517d765797f90e7d54bd17
piotrwojtaszek <37346684+pietrek15@users.noreply.github.com>
2019-06-24 00:49:27 +0200

    Last

---
+++ Assets/InteractObjectDetect.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractObjectDetect : MonoBehaviour
+{
+    public Transform center;
+    // Update is called once per frame
+    void Update()
+    {
+
+        Active();
+    }
+
+    void Active()
+    {
+        if(Camera.main.transform.position == center.position)
+        {
+            foreach(Transform child in transform)
+            {
+                child.gameObject.SetActive(true);
+            }
+        }
+        else
+        {
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
+    }
+}
